89b940eb3fffb996db2dab16ef72a2e144b1ce9e
clemgrass <cgrassdev@gmail.com>
2026-04-07 13:45:20 -0400

    feat: room compisionist mesh

--- titulacion-khachkar/Museum/Room_Complisonist/crowbar.gd
+++ titulacion-khachkar/Museum/Room_Complisonist/crowbar.gd
@@ -4,5 +4,5 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func grab_crowbar():
 	$"..".visible = false
-	self.use_collision = false
+	$coll_crow.disabled = true
 	
