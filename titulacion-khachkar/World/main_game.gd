7c6a4bd0eff20f888cf7033f317108ebdc1cb22d
clemgrass <cgrassdev@gmail.com>
2026-05-03 16:16:55 -0400

    feat: backdoor

--- titulacion-khachkar/World/main_game.gd
+++ titulacion-khachkar/World/main_game.gd
@@ -35,7 +35,7 @@ func _ready() -> void:
 	await get_tree().process_frame
 	
 	var puzzle = get_tree().get_first_node_in_group("puzzle0")
-	var door = get_tree().get_first_node_in_group("door")
+	var doors = get_tree().get_nodes_in_group("door")
 	
 	var khachkars = get_tree().get_nodes_in_group("khachkars")
 	var room_khachkars = get_tree().get_first_node_in_group("room_khachkars")
@@ -44,14 +44,16 @@ func _ready() -> void:
 	var final_door = get_tree().get_first_node_in_group("final_door")
 	
 	var player = get_tree().get_first_node_in_group("player")
-
+	
+	for door in doors:
+		puzzle.puzzle0_solved.connect(door._on_puzzle_solved)
+		
 	for khachkar in khachkars:
 		khachkar.puzzle_solved.connect(room_khachkars._on_puzzle_solved)
 		khachkar.puzzle_solved.connect(album._on_puzzle_solved)
 	
 	room_khachkars.all_solved.connect(final_door._on_all_solved)
-	
-	puzzle.puzzle0_solved.connect(door._on_puzzle_solved)
+
 	puzzle.puzzle0_solved.connect(album._on_puzzle_solved)
 	puzzle.puzzle0_solved.connect(player._on_puzzle_solved)
 	
