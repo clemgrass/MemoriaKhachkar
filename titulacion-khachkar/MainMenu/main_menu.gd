b32a0dcd0b07db19e31bab9aa1cce32246006add
clemgrass <cgrassdev@gmail.com>
2026-05-03 18:45:13 -0400

    fix: capture mouse ending

--- titulacion-khachkar/MainMenu/main_menu.gd
+++ titulacion-khachkar/MainMenu/main_menu.gd
@@ -9,6 +9,7 @@ var is_loaded := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ResourceLoader.load_threaded_request(GAME_SCENE)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
